5ea2eae3ced8ad37eb0b994eae9a8fb729da15f8
Vidit Dhull <vidit.dh@gmail.com>
2023-08-27 14:30:44 +0530

    deploy

--- website/settings.py
+++ website/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-4t*d7!6&^x)tnz6nxi&7f3)efcv0ix+q0l55++ad7_##5^)1*#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["http://luciustensors.com", "luciustensors.com"]
 
 
 # Application definition
