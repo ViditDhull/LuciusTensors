40d1b3e7c187d148f1084fe694e4ef77970bc944
Vidit Dhull <vidit.dh@gmail.com>
2023-11-01 17:40:44 +0530

    Path update

--- utils/query_pdf.py
+++ utils/query_pdf.py
@@ -26,14 +26,14 @@ def pdf_query_generator(pdf, query):
 
         
         file_name = pdf.name[:-4]
-
-        if os.path.exists(f"PDF_Embeddings/{file_name}.pkl"):
-            with open(f"PDF_Embeddings/{file_name}.pkl", 'rb') as f:
+        file_path = os.path.join("PDF_Embeddings", f"{file_name}.pkl")
+        if os.path.exists(file_path):
+            with open(file_path, 'rb') as f:
                 vec_store = pickle.load(f)
         else:
             embeddings = OpenAIEmbeddings(openai_api_key=api_key)
             vec_store = FAISS.from_texts(chunks, embeddings)
-            with open(f"PDF_Embeddings/{file_name}.pkl", "wb") as f:
+            with open(file_path, "wb") as f:
                 pickle.dump(vec_store, f)
 
         if query:
